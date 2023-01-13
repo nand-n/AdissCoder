1c9ea6169db7658314c88b3c461dd0ef3fea4850
nand-n <nahomdebele002@gmail.com>
2023-01-14 02:06:34 +0300

    on some progress but have a huge bug

--- adiss_codder/src/App.jsx
+++ adiss_codder/src/App.jsx
@@ -1,33 +1,99 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import TodoInput from './Components/Todoinput';
+import TodoList from './Components/TodoList';
+import axios from "axios";
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  state = {
+    todoList: [],
+    activeItem: {
+      title: "",
+      completed: false,
+    },
+    editItem: false,
+  };
+
+  
+useEffect(()=>{
+  refreshList()
+},[])
+  // componentDidMount() {
+  //   this.refreshList();
+  // };
+
+  refreshList = () => {
+    axios
+      .get("localhsot:3000/api/v1/tasks")
+      .then((res) => this.setState({ todoList: res.data.tasks }))
+      .catch((err) => console.log(err));
+  };
+
+  handleChange = (e) => {
+    let { name, value } = e.target;
+
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
+
+    const activeItem = { ...this.state.activeItem, [name]: value };
+
+    this.setState({ activeItem });
+  };
+
+
+  handleSubmit = (item) => {
+    this.setState({
+      editItem: false,
+    });
+    // alert("Save :: " + JSON.stringify(item));
+    if (item._id) {
+      axios
+        .patch(`/api/v1/tasks/${item._id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    axios.post("/api/v1/tasks/", item).then((res) => this.refreshList());
+  };
+
+  handleEdit = (item) => {
+    this.setState({ activeItem: item, editItem: true });
+    // alert("Edit :: " + JSON.stringify(item));
+  };
+
+  handleDelete = (item) => {
+    // alert("Delete :: " + JSON.stringify(item));
+    axios
+      .delete(`/api/v1/tasks/${item._id}/`)
+      .then((res) => this.refreshList());
+  };
+
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    // <div className="App">
+       <div className="container">
+        <h1 className="text-uppercase text-center my-2">Todo App</h1>
+        <div className="row">
+          <div className="col-8 col-md-6 mx-auto">
+            <TodoInputnput
+              activeItem={this.state.activeItem}
+              editItem={this.state.editItem}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <TodoListt
+              items={this.state.todoList}
+              handleEdit={this.handleEdit}
+              handleDelete={this.handleDelete}
+            />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+     
+    // </div>
   )
 }
 
